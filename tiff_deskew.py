9bab5d7228ef8b2cc10b48843abc301fa5658f19
F-S-Menezes <f.menezes@uq.net.au>
2022-02-04 08:45:28 +0000

    cleaned up tiff_deskew.py, new functionality not present in this commit. effectiveley the same as last commit. diditbreak.sh, bash script for Hoang, need to change the .tiff file used in the singularity line to whatever other .tiff file you wish to use

--- tiff_deskew.py
+++ tiff_deskew.py
@@ -1,19 +1,13 @@
 from pathlib import Path
-import numpy as np
 from skimage.io import imsave,imread
 import os
 
-from pycudadecon.affine import affineGPU, deskewGPU
-#from pycudadecon.util import imread
+from pycudadecon.affine import deskewGPU
 
 def main(im_path):
 	"test that we can actually deskew an image"
 	im=imread(str(im_path))
 
-	im_z, im_y, im_x =im.shape
-#	TMAT = np.array([[1, 0, 0, im_x], [0, 1, 0, im_y], [0, 0, 1, im_z], [0, 0, 0, 1]])
-	
-#	out = affineGPU(im, TMAT)
 	out = deskewGPU(im)
 	
 	dirname, fname = os.path.split(im_path)
@@ -21,13 +15,7 @@ def main(im_path):
 	outdir = os.path.join(dirname, fname + "_deskewed")
 	os.makedirs(outdir, exist_ok=True)
 	dst = os.path.join(outdir, f"{im_path}_deskewed.tif")
-	
-#	print(outdir)
-#	print(dst)
-#	print(fname)
-#	print(im_path)
-#	print(im.shape)
-	
+		
 	imsave(dst, out, imagej=True)
 	
 	
