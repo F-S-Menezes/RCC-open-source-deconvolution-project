f1c1ad53923f85208460dd8aee68d13a46852f2d
F-S-Menezes <f.menezes@uq.net.au>
2022-02-14 08:40:47 +0000

    in-progress update to tiff_test.py (pending rename) main now optionally only deskews if only provided with a raw_image,  if given a psf, proceeds to deconvolve (still implementing)

--- tiff_test.py
+++ tiff_test.py
@@ -7,11 +7,11 @@ from pycudadecon.affine import affineGPU, deskewGPU
 from pycudadecon import decon, rl_cleanup, rl_decon, rl_init
 #from pycudadecon.util import imread
 
-class Testing:
+class Process:
 	"""class structure to aid with functions that deskew and/or deconvolve some .tif/.tiff image
 	"""
 	
-	def __init__(self, im_path, mode):
+	def __init__(self, im_path, psf_path=None):
 		"""initializes some frequently used properties
 	
 		Params:
@@ -19,56 +19,66 @@ class Testing:
 			mode (bool): 0 to only deskew the image or 1 to also deconvolve it, saves seperately
 		"""
 		self._im_path = im_path
-		self._mode = mode
 		self._im = imread(str(im_path))
-		dirname, self._fname = os.path.split(im_path) #the original filename that spawned files will piggy-back their names off of
-		self._fname, ext = os.path.splitext(self._fname)
-		self._outdir = os.path.join(dirname, self._fname + "_pycudadecon")
-		os.makedirs(self._outdir, exist_ok=True)
+		self._psf_path = psf_path
+		self._otf = None
+#		dirname, self._fname = os.path.split(im_path) #the original filename, that spawned files will piggy-back their names off of
+#		self._fname, ext = os.path.splitext(self._fname)
+#		self._outdir = os.path.join(dirname, self._fname + "_pycudadecon")
+#		os.makedirs(self._outdir, exist_ok=True)
 		
 	def run(self):
-		#	im_z, im_y, im_x =im.shape
-#		TMAT = np.array([[1, 0, 0, im_x], [0, 1, 0, im_y], [0, 0, 1, im_z], [0, 0, 0, 1]])
 	
-#		out = affineGPU(im, TMAT)
 		out = deskewGPU(self._im)
-		self.save_file(out)
+		print("wtf is this: ", type(out))
+#		self.save_file(out)
 	
-#		if self._mode:
+		if self._psf_path is not None:
+			print("decon")
+			########working on this section
 #			rl_init(deskewed.shape, im_path) 
 #			print(deskewed.shape)
 #			out = rl_decon(deskewed, background=100)
 #			self.save_file(out, im_path, outdir)
 #			rl_cleanup()
+		else:
+			print("deskew only")
 	
 	def save_file(self, out):
-		"""saves the file im to a   new folder and returns the path to the saved file.
+		"""saves the file 'out' to the pycudadecon folder of this process
 
 		PARAMETERS:
-			im (imread object): the file to be saved
-			im_path (string): path to the image to be saved
-			outdir (string): used when deconvolving so that we reuse the same folder created when saving the deskewed image
+			out (numpy.ndarray): an image processed by pycudadecon to be saved
 		"""
-		if self._mode == 1:
+		if self._psf is not None:
+			dst = os.path.join(self._outdir, "otf.tiff")
+		else if self._otf is not None:
 			dst = os.path.join(self._outdir, f"{self._fname}_deconvolved.tiff")
 		else:
 			dst = os.path.join(self._outdir, f"{self._fname}_deskewed.tiff")
 		imsave(dst, out, imagej=True)
-#		return(outdir, new_file)
 
-def main(im_path, mode):
+def main(args):
 	"""test deskew and/or deconvolve
 	
 	PARAMETERS:
 	im_path (string): path to the image
 	decon (bool): 0 to only deskew the image or 1 to also deconvolve it, saves seperately
 	"""
-	data = Testing(im_path, mode)
+	if len(args)>2 or len(args)==0:
+		print("WARNING: incorrect number of args\nusage:\nto deskew:\tpython3 tiff_xxxx raw_file.tif/tiff\nto deconvolve:\tpython3 tiff_xxxx raw_file.tif/tiff instrument_psf.tif/tiff")
+	print("\nspacing\n")
+	
+	im_path = args[0]
+	if len(args)>1:
+		psf_path = args[1]
+		data = Process(im_path, psf_path)
+	else:
+		data = Process(im_path)
+
 	data.run()
 	
 if __name__ == "__main__":
 	import sys
 	
-#	print(sys.argv[1])
-#	print(sys.argv[2])
-	main(sys.argv[1], sys.argv[2])
+	main(sys.argv[1:])
