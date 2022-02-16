45fc718a1bc157bae9ed4c423086fab6eeae6737
F-S-Menezes <f.menezes@uq.net.au>
2022-02-16 05:40:17 +0000

    tiff_test.py (rename pending) succesfuly deskews test images (so far), may require some tuning to UQ systems (up next). want to either make the otf generation it's own function and then simply add a line to the the bash script for each file to deconvolve OR i can make it so the function solely takes the path to a folder of images ( and optionally a psf) and processes all the images found wherein

--- tiff_test.py
+++ tiff_test.py
@@ -3,15 +3,15 @@ import numpy as np
 from skimage.io import imsave,imread
 import os
 
-from pycudadecon.affine import affineGPU, deskewGPU
-from pycudadecon import decon, rl_cleanup, rl_decon, rl_init
+from pycudadecon.affine import deskewGPU
+from pycudadecon import decon, rl_cleanup, rl_decon, rl_init, make_otf
 #from pycudadecon.util import imread
 
 class Process:
 	"""class structure to aid with functions that deskew and/or deconvolve some .tif/.tiff image
 	"""
 	
-	def __init__(self, im_path, psf_path=None):
+	def __init__(self, im_path, psf=None):
 		"""initializes some frequently used properties
 	
 		Params:
@@ -20,42 +20,39 @@ class Process:
 		"""
 		self._im_path = im_path
 		self._im = imread(str(im_path))
-		self._psf_path = psf_path
+		self._psf = psf
 		self._otf = None
-#		dirname, self._fname = os.path.split(im_path) #the original filename, that spawned files will piggy-back their names off of
-#		self._fname, ext = os.path.splitext(self._fname)
-#		self._outdir = os.path.join(dirname, self._fname + "_pycudadecon")
-#		os.makedirs(self._outdir, exist_ok=True)
+		dirname, self._fname = os.path.split(im_path) #the original filename, that spawned files will piggy-back their names off of
+		self._fname, ext = os.path.splitext(self._fname)
+		self._outdir = os.path.join(dirname, self._fname + "_pycudadecon")
+		os.makedirs(self._outdir, exist_ok=True)
 		
 	def run(self):
 	
-		out = deskewGPU(self._im)
-		print("wtf is this: ", type(out))
-#		self.save_file(out)
+		deskewed = deskewGPU(self._im)
 	
-		if self._psf_path is not None:
-			print("decon")
-			########working on this section
-#			rl_init(deskewed.shape, im_path) 
-#			print(deskewed.shape)
-#			out = rl_decon(deskewed, background=100)
-#			self.save_file(out, im_path, outdir)
-#			rl_cleanup()
+		if self._psf is not None:
+			print("\ngenerating otf")
+			make_otf(self._psf, f"{self._outdir}/{self._fname}_otf.tiff")
+			
+			print("deconvolving image")
+			rl_init(deskewed.shape, f"{self._outdir}/{self._fname}_otf.tiff", ) 
+			deconvolved = rl_decon(deskewed, background='auto')
+			self.save_file(deconvolved, "deconvolved")
+			rl_cleanup()
 		else:
 			print("deskew only")
+			self.save_file(deskewed, "deskewed")
+			
 	
-	def save_file(self, out):
+	def save_file(self, out, save_as):
 		"""saves the file 'out' to the pycudadecon folder of this process
 
 		PARAMETERS:
 			out (numpy.ndarray): an image processed by pycudadecon to be saved
+			save_as(string): what to name the given file
 		"""
-		if self._psf is not None:
-			dst = os.path.join(self._outdir, "otf.tiff")
-		else if self._otf is not None:
-			dst = os.path.join(self._outdir, f"{self._fname}_deconvolved.tiff")
-		else:
-			dst = os.path.join(self._outdir, f"{self._fname}_deskewed.tiff")
+		dst = os.path.join(self._outdir, f"{self._fname}_{save_as}.tiff")
 		imsave(dst, out, imagej=True)
 
 def main(args):
@@ -80,5 +77,5 @@ def main(args):
 	
 if __name__ == "__main__":
 	import sys
-	
+
 	main(sys.argv[1:])
